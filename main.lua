2834e12b97494c033db56c1ced278d9ae0c21704
Axel Dultinger <axel@dultinger.net>
2024-04-05 13:19:45 +0200

    (V1.1.0) made the item stackable

--- main.lua
+++ main.lua
@@ -1,40 +1,46 @@
 local mod = RegisterMod("Tech Infinity", 1)
 local TECH_INFINITY_ID = Isaac.GetItemIdByName("Tech Infinity")
+--declarations
 local GIVE = "giveitem c"
 local REMOVE = "remove c"
-local lastItem = 0
+local lastItem = {}
+    local id = 0
+for j=1, 1000 do
+    lastItem[j] = 0
+end
+
 local game = Game()
 
 function mod:randTech()
-    if lastItem ~= 0 then
-        Isaac.ExecuteCommand(REMOVE..lastItem)
-    end
-    local playercount = game:GetNumPlayers()
+    
 
+    local playercount = game:GetNumPlayers()
     for playerIndex = 0, playercount - 1 do
         local player = Isaac.GetPlayer(playerIndex)
         local itemcount = player:GetCollectibleNum(TECH_INFINITY_ID)
 
-        if itemcount > 0 then
-            local rng = player:GetCollectibleRNG(TECH_INFINITY_ID)
-            local id = 1
+        for  i = 1, itemcount , 1 do
+            local rng = player:GetCollectibleRNG(TECH_INFINITY_ID) --initializes the rng with the player's seed
 
-            local rand = rng:RandomInt(5)
+            local rand = rng:RandomInt(5) --generates a random integer between 0-4
             
-            if rand == 0 then
-            id = 68
+            if rand == 0 then   --basically a switch statement that chooses a tech item with the random number 
+            id = CollectibleType.COLLECTIBLE_TECHNOLOGY
             elseif rand == 1 then 
-            id = 152
+            id = CollectibleType.COLLECTIBLE_TECHNOLOGY_2
             elseif rand == 2 then 
-            id = 244
+            id = CollectibleType.COLLECTIBLE_TECH_5
             elseif rand == 3 then 
-            id = 395
+            id = CollectibleType.COLLECTIBLE_TECH_X
             elseif rand == 4 then 
-            id = 524
+            id = CollectibleType.COLLECTIBLE_TECHNOLOGY_ZERO
             end 
 
-            lastItem = id
-            Isaac.ExecuteCommand(GIVE..id)
+            if lastItem[i] ~= 0 then 
+                Isaac.ExecuteCommand(REMOVE..lastItem[i])
+            end
+            lastItem[i] = id   --remembers the id of the last given item
+            Isaac.ExecuteCommand(GIVE..id) --executes the console command to give isaac the item
         end
     end
 end
